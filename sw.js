75f1bf6f8d339f77c1734bc465aaddce1913f804
carlessp <carles.hm@gmail.com>
2024-09-24 12:54:08 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -9,6 +9,21 @@ const urlsToCache = [
 
 // Rest of your service worker code remains the same
 
+self.addEventListener('activate', event => {
+    const cacheWhitelist = [CACHE_NAME];
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cacheName => {
+                    if (!cacheWhitelist.includes(cacheName)) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
+
 self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME)
